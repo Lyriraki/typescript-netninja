416e67d70d80e92ec8c507e5f6b86c60b8034c21
Lyriraki <Luckyarif21@gmail.com>
2021-02-04 00:11:16 +0700

    types and object

--- sandbox.ts
+++ sandbox.ts
@@ -2,4 +2,31 @@ const circ = (diameter: number) => {
     return diameter * Math.PI
 }
 
-console.log(circ(12));
+console.log(circ(12.3));
+
+//* Array
+let numbers = [1,2,3,4,5]
+numbers.push(2);
+// numbers = 2 //! Can't change the type of data
+// numbers.push('Lucky'); //! Static
+// numbers[2] = "Arif"; //! Static
+
+let mixed = ['Lucky', 22, "Arif", true]
+mixed.push(false)
+mixed.push(31)
+mixed.push("Arif")
+
+//* Object
+let ninja = {
+    name: 'Mario',
+    belt: 'Red',
+    age: 30
+}
+// ninja.age = 'hello' //! static
+// ninja.skills = ['Fighting', 'Genjutsu'] //! Cant add more property
+// ninja = {
+//     name: 'Mario',
+//     belt: 'Red',
+//     age: 30,
+//     skills : ['Fighting', 'Genjutsu']
+// } //! must be always same with the first declaration
